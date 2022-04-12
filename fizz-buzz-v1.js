7ac97531003f111bc4353e0ad6a49d9ef03b0371
desarrollonetcardenas <desarrollo.net.cardenas@hotmail.com>
2022-04-12 16:45:38 -0600

    fizz buzz v1

---
+++ fizz-buzz-v1.js
@@ -0,0 +1,20 @@
+
+function fizzBuzz(n) {
+    if (n <= 0)
+        return;
+
+    for (let i = 1; i <= n; i++) {
+        if ((i % 3) === 0 && (i % 5) === 0) {
+            console.log('FizzBuzz');
+        } else if (i % 3 === 0){
+            console.log('Fizz');
+        } else if (i % 5 === 0) {
+            console.log('Buzz');
+        } else console.log(i);
+    }
+    
+}
+
+// fizzBuzz(15);
+// fizzBuzz(0);
+fizzBuzz(1000000);
