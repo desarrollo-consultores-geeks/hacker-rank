a6efdc35a68bf3012cb8820e11160e4411e2929e
desarrollonetcardenas <desarrollo.net.cardenas@hotmail.com>
2022-04-12 17:30:25 -0600

    fizz buzz v2

--- fizz-buzz-v2.js
+++ fizz-buzz-v2.js
@@ -1,4 +1,25 @@
+/**
+ * Fizz Buzz function that outputs the value that is
+ * multiple of the parameter
+ * @param {*} n 
+ * @param {*} multipleNumber 
+ * @returns nothing
+ */
+function fizzBuzz(n, multipleNumber = []) {
+    let i = 0;
+    if (n <= 0)
+        return;
 
-function fizzBuzz(n, ) {
+    if (multipleNumber === [])
+        return;
 
+    while (i++ < n) {
+        // console.log(i);
+        multipleNumber.map((value, index, number) => {
+            if (i % value === 0)
+                console.log(`${i} is multiple of ${value}`);
+        });
+    }
 }
+
+fizzBuzz(15, [3, 5]);
